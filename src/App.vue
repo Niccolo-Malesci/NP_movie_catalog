8d494be4ff388b26bde54d087b9d978717c161bc
Niccolo-Malesci <132044639+Niccolo-Malesci@users.noreply.github.com>
2023-09-08 09:24:04 +0200

    codice aggiornato

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
+# NP_movie_catalog
+
+
 <template>
   <div>
-    <h1 class="catalog-title">{{ currentLanguage === 'it' ? 'Catalogo di Film' : 'Movie Catalog' }}</h1>
+    <h1 class="catalog-title">{{ translations[currentLanguage].catalogTitle }}</h1>
     <input v-model="searchQuery" @input="handleSearchInput" placeholder="Search..." class="search-input" />
     <div v-if="movies.length" class="movie-list">
       <ul>
@@ -8,20 +11,23 @@
           <img :src="getMoviePosterUrl(movie.poster_path)" alt="Movie Poster" class="movie-poster" />
           <h2 class="movie-title">{{ movie.title }}</h2>
           <p class="movie-release-date">{{ movie.release_date }}</p>
-          <p class="movie-overview">{{ getTranslatedOverview(movie.overview, currentLanguage) }}</p>
+          <p class="movie-overview">{{ movie.overview }}</p>
         </li>
       </ul>
       <div class="pagination-buttons">
-        <button @click="fetchPrevMovies" v-if="currentPage > 1" class="pagination-button">{{ currentLanguage === 'it' ? 'Pagina precedente' : 'Previous Page' }}</button>
-        <button @click="fetchNextMovies" v-if="currentPage < totalPages" class="pagination-button">{{ currentLanguage === 'it' ? 'Pagina successiva' : 'Next Page' }}</button>
+        <button @click="fetchPrevMovies" v-if="currentPage > 1" class="pagination-button">{{ translations[currentLanguage].prevPage }}</button>
+        <button @click="fetchNextMovies" v-if="currentPage < totalPages" class="pagination-button">{{ translations[currentLanguage].nextPage }}</button>
       </div>
     </div>
     <div v-else>
-      <p class="loading-text">{{ currentLanguage === 'it' ? 'Caricamento in corso...' : 'Loading...' }}</p>
+      <p class="loading-text">{{ translations[currentLanguage].loadingText }}</p>
     </div>
     <div>
-      <button @click="switchToItalian" v-if="currentLanguage === 'en'">Switch to Italian</button>
-      <button @click="switchToEnglish" v-if="currentLanguage === 'it'">Switch to English</button>
+      <button @click="toggleLanguage">{{ currentLanguage === 'en' ? 'Switch to Italian' : 'Switch to English' }}</button>
+    </div>
+    <div>
+      <button @click="setCurrentCategory('movie')" :class="{ 'active': currentCategory === 'movie' }">Film</button>
+      <button @click="setCurrentCategory('tv')" :class="{ 'active': currentCategory === 'tv' }">Serie TV</button>
     </div>
   </div>
 </template>
@@ -42,19 +48,18 @@ export default {
           catalogTitle: 'Catalogo di Film',
           prevPage: 'Pagina precedente',
           nextPage: 'Pagina successiva',
-          movieDescriptions: {
-          },
+          movieDescriptions: {},
         },
         en: {
           loadingText: 'Loading...',
           catalogTitle: 'Movie Catalog',
           prevPage: 'Previous Page',
           nextPage: 'Next Page',
-          movieDescriptions: {
-          },
+          movieDescriptions: {},
         },
       },
-      searchQuery: '', 
+      searchQuery: '',
+      currentCategory: 'movie',
     };
   },
   created() {
@@ -65,67 +70,79 @@ export default {
       const apiKey = '512f81af17888b517a1b456fbce07689';
       const language = this.currentLanguage;
       const page = this.currentPage;
-      const query = this.searchQuery; 
+      const query = this.searchQuery;
+      const category = this.currentCategory;
+
       axios
-        .get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=${language}&page=${page}&query=${query}`)
+        .get(
+          `https://api.themoviedb.org/3/search/${category}?api_key=${apiKey}&language=${language}&page=${page}${
+            query ? `&query=${query}` : ''
+          }`
+        )
         .then((response) => {
           this.totalPages = response.data.total_pages;
           this.movies = response.data.results.map((movie) => {
-            const translatedOverview = this.getTranslatedOverview(movie.overview, this.currentLanguage);
             return {
               ...movie,
-              overview: translatedOverview,
+              overview: this.getTranslatedOverview(movie.overview, this.currentLanguage),
             };
           });
         })
         .catch((error) => {
-          console.error('Errore durante il recupero dei film:', error);
+          console.error('Errore durante il recupero dei film/serie TV:', error);
         });
     },
+
+    setCurrentCategory(category) {
+      this.currentCategory = category;
+      this.fetchMovies();
+    },
+
     fetchPrevMovies() {
       if (this.currentPage > 1) {
         this.currentPage--;
         this.fetchMovies();
       }
     },
+
     fetchNextMovies() {
       if (this.currentPage < this.totalPages) {
         this.currentPage++;
         this.fetchMovies();
       }
     },
+
+    toggleLanguage() {
+      this.currentLanguage = this.currentLanguage === 'en' ? 'it' : 'en';
+      this.fetchMovies();
+    },
+
     getMoviePosterUrl(posterPath) {
+      if (!posterPath) {
+        return '';
+      }
       return `https://image.tmdb.org/t/p/w500/${posterPath}`;
     },
-    switchToEnglish() {
-      this.currentLanguage = 'en';
-      this.fetchMovies();
-    },
-    switchToItalian() {
-      this.currentLanguage = 'it';
-      this.fetchMovies();
-    },
+
     getTranslatedOverview(overview, language) {
-      const languageTranslations = this.translations[language];
-
-      if (languageTranslations && languageTranslations.movieDescriptions[overview]) {
-        return languageTranslations.movieDescriptions[overview];
-      }
-
       return overview;
     },
+
     handleSearchInput() {
-      this.currentPage = 1; 
-      this.fetchMovies(); 
+      this.currentPage = 1;
+      this.fetchMovies();
     },
   },
 };
 </script>
 
+
 <style scoped>
 .catalog-title {
-  font-size: 24px;
-  margin-bottom: 20px;
+  font-size: 36px;
+  margin-bottom: 40px;
+  text-align: center;
+  color: #fff;
 }
 
 .movie-list {
@@ -135,52 +152,88 @@ export default {
 
 .movie-item {
   display: flex;
-  margin-bottom: 20px;
-  align-items: center;
+  margin-bottom: 40px;
+  align-items: flex-start;
+  background-color: rgba(0, 0, 0, 0.7);
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
 }
 
 .movie-poster {
   max-width: 150px;
   margin-right: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
 }
 
 .movie-title {
-  font-size: 20px;
+  font-size: 24px;
   margin: 0;
+  color: #fff;
 }
 
 .movie-release-date {
-  font-size: 16px;
+  font-size: 18px;
   margin: 0;
+  color: #ccc;
 }
 
 .movie-overview {
-  font-size: 14px;
+  font-size: 16px;
+  color: #fff;
 }
 
 .pagination-buttons {
   margin-top: 20px;
+  text-align: center;
 }
 
 .pagination-button {
-  background-color: #007bff;
+  background-color: #e50914;
   color: #fff;
   border: none;
   padding: 10px 20px;
   margin-right: 10px;
   cursor: pointer;
   border-radius: 5px;
-  font-size: 14px;
+  font-size: 16px;
 }
 
 .loading-text {
-  font-size: 16px;
+  font-size: 24px;
   margin: 20px 0;
+  text-align: center;
+  color: #fff;
 }
 
 .search-input {
   margin-bottom: 20px;
-  padding: 5px 10px;
-  font-size: 14px;
+  padding: 10px;
+  font-size: 18px;
+  width: 100%;
+  border: none;
+  border-radius: 5px;
+}
+
+button.active {
+  background-color: #e50914;
+  font-weight: bold;
+  color: #fff;
+}
+
+body {
+  background-color: #141414;
+  font-family: Arial, sans-serif;
+  color: #fff;
+  margin: 0;
+  padding: 0;
+}
+
+footer {
+  text-align: center;
+  padding: 20px;
+  background-color: #000;
+  color: #fff;
 }
 </style>
