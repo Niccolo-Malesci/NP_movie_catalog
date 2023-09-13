f4b6c06af5acbd03290dbb5f379019e6244d4482
DiodoVaricap <thegameryt050@gmail.com>
2023-09-13 14:52:31 +0200

    r

--- src/views/Catalogo.vue
+++ src/views/Catalogo.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid" style="width: max-content;">
-    <a class="navbar-brand" href="#" style="color: red; font-family: fantasy;">{{ $t('appTitle') }}</a>
+    <a class="navbar-brand" href="#" style="color: red; font-family: fantasy; font-size: xx-large;">{{ $t('appTitle') }}</a>
   </div>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
