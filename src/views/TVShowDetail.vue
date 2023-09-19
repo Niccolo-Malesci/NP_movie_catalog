67f6fd743d2412f4ed4e389c1a308a06b845edb5
Niccolò <niccomalesci@gmail.com>
2023-09-19 17:35:01 +0200

    ..

--- src/views/TVShowDetail.vue
+++ src/views/TVShowDetail.vue
@@ -1,73 +1,84 @@
 <template>
     <div class="main">
-      <Navbar @language-change="changeLanguage" />
-      <MediaDetailCard :media="tvShow" />
-      <MediaCarousel
-        :chunkedRecommendedMedia="chunkedRecommendedTvShows"
-        :currentSlideIndex="currentSlideIndex"
-        currentCategory="tv"
-      />
+        <Navbar @language-change="changeLanguage" />
+        <MediaDetailCard :media="tvShow" />
+        <MediaCarousel :chunkedRecommendedMedia="chunkedRecommendedTvShows" :currentSlideIndex="currentSlideIndex"
+            currentCategory="tv" />
     </div>
-  </template>
+</template>
   
-  <script>
-  import Navbar from '../components/Navbar.vue';
-  import MediaDetailCard from '../components/MediaDetailCard.vue';
-  import MediaCarousel from '../components/MediaCarousel.vue';
-  
-  export default {
+<script>
+import Navbar from '../components/Navbar.vue';
+import MediaDetailCard from '../components/MediaDetailCard.vue';
+import MediaCarousel from '../components/MediaCarousel.vue';
+
+export default {
     data() {
-      return {
-        apiKey: import.meta.env.VITE_API_KEY,
-        language: 'it',
-        tvShow: {},
-        recommendedTvShows: [],
-        chunkedRecommendedTvShows: [],
-        currentSlideIndex: 0,
-        chunkSize: 5,
-      };
+        return {
+            apiKey: import.meta.env.VITE_API_KEY,
+            language: 'it',
+            tvShow: {},
+            recommendedTvShows: [],
+            chunkedRecommendedTvShows: [],
+            currentSlideIndex: 0,
+            chunkSize: 5,
+        };
     },
-    created() {
-      const tvShowId = this.$route.params.id;
-      const tvShowUrl = `https://api.themoviedb.org/3/tv/${tvShowId}?api_key=${this.apiKey}&language=${this.language}`;
-      const recommendedTvShowsUrl = `https://api.themoviedb.org/3/tv/${tvShowId}/recommendations?api_key=${this.apiKey}&language=${this.language}`;
-  
-      fetch(tvShowUrl)
-        .then(response => response.json())
-        .then(data => {
-          this.tvShow = data;
-        })
-        .catch(error => console.error('Errore durante il recupero dei dettagli della serie TV:', error));
-  
-      fetch(recommendedTvShowsUrl)
-        .then(response => response.json())
-        .then(data => {
-          this.recommendedTvShows = data.results;
-          this.chunkedRecommendedTvShows = this.chunkArray(this.recommendedTvShows, this.chunkSize);
-        })
-        .catch(error => console.error('Errore durante il recupero delle serie TV consigliate:', error));
+    async created() {
+        await this.fetchGetTvShow();
     },
     computed: {
-      currentChunkRecommendedTvShows() {
-        return this.chunkedRecommendedTvShows[this.currentSlideIndex] || [];
-      },
+        currentChunkRecommendedTvShows() {
+            return this.chunkedRecommendedTvShows[this.currentSlideIndex] || [];
+        },
+    },
+    watch: {
+        "$route.params": {
+            handler({ id } = {}) {
+                if (!id) return;
+                this.fetchGetTvShow();
+            },
+            deep: true,
+            immediate: true,
+        },
     },
     methods: {
-      chunkArray(array, size) {
-        const chunkedArray = [];
-        for (let i = 0; i < array.length; i += size) {
-          chunkedArray.push(array.slice(i, i + size));
-        }
-        return chunkedArray;
-      },
+        async fetchGetTvShow() {
+            const tvShowId = this.$route.params.id;
+            const tvShowUrl = `https://api.themoviedb.org/3/tv/${tvShowId}?api_key=${this.apiKey}&language=${this.language}`;
+            const recommendedTvShowsUrl = `https://api.themoviedb.org/3/tv/${tvShowId}/recommendations?api_key=${this.apiKey}&language=${this.language}`;
+
+            fetch(tvShowUrl)
+                .then(response => response.json())
+                .then(data => {
+                    this.tvShow = data;
+                })
+                .catch(error => console.error('Errore durante il recupero dei dettagli della serie TV:', error));
+
+            fetch(recommendedTvShowsUrl)
+                .then(response => response.json())
+                .then(data => {
+                    this.recommendedTvShows = data.results;
+                    this.chunkedRecommendedTvShows = this.chunkArray(this.recommendedTvShows, this.chunkSize);
+                })
+                .catch(error => console.error('Errore durante il recupero delle serie TV consigliate:', error));
+        },
+
+        chunkArray(array, size) {
+            const chunkedArray = [];
+            for (let i = 0; i < array.length; i += size) {
+                chunkedArray.push(array.slice(i, i + size));
+            }
+            return chunkedArray;
+        },
     },
     components: { Navbar, MediaDetailCard, MediaCarousel },
-  };
-  </script>
+};
+</script>
   
-  <style scoped>
-  .main {
+<style scoped>
+.main {
     margin-top: 87px;
-  }
-  </style>
+}
+</style>
   
