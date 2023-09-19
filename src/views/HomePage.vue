0ba70bae21eeab4878ec1f13669294d69f9c2ad7
Niccolò <niccomalesci@gmail.com>
2023-09-19 17:52:14 +0200

    ..

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h1 class="saluto">Ciao {{ nome }}!</h1>
         <h2 class="presentazione">Scegli i tuoi generi preferiti:</h2>
-        <div class="row align-items-start justify-content-center">
+        <div style="margin-left: 12%;" class="row align-items-start justify-content-center">
             <div style="margin-top: 3%;" v-for="genere in generi" :key="genere" class="col-md-3">
                 <div class="form-check">
                     <input style="zoom: 1.5;" class="form-check-input" type="checkbox" :value="genere"
