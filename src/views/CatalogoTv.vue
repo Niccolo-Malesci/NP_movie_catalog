6b457414743b877c7f82fc27d04af6a36656e977
DiodoVaricap <thegameryt050@gmail.com>
2023-09-20 10:56:16 +0200

    uik

--- src/views/CatalogoTv.vue
+++ src/views/CatalogoTv.vue
@@ -24,7 +24,7 @@
         </div>
       </div>
     </div>
-    <ul v-if="showPagination" class="pagination justify-content-center" style="margin-top: 1%;">
+    <ul class="pagination justify-content-center" style="margin-top: 1%;">
       <li class="page-item">
         <a @click="fetchPrevMovies" v-if="currentPage > 1" class="page-link">Pagina precedente</a>
       </li>
@@ -57,7 +57,6 @@ export default {
       currentLanguage: 'it',
       searchQuery: '',
       currentCategory: 'movie',
-      showPagination: true
     };
   },
   mounted() {
@@ -166,7 +165,6 @@ export default {
       this.searchQuery = query;
       this.currentPage = 1;
       this.fetchMovies();
-      this.showPagination = false
     },
     changeLanguage(newLanguage) {
       this.currentLanguage = newLanguage;
