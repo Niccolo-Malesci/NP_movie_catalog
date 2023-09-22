fa5709f6ab9ed4f66e8d89eb37df04ba0cbf8a81
DiodoVaricap <thegameryt050@gmail.com>
2023-09-22 10:30:58 +0200

    68

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,9 +2,19 @@
     <div class="container">
         <h1 class="saluto">{{ $t('hello') }} {{ nome }}!</h1>
         <h2 class="presentazione">{{ $t('chooseGenres') }}</h2>
-        <h3 class="presentazione">{{ $t('preferiti') }}<i class="bi bi-arrow-through-heart-fill"></i>  "</h3>
+        <h3 class="presentazione">{{ $t('preferiti') }}<i class="bi bi-arrow-through-heart-fill"></i> "</h3>
         <div style="margin-left: 12%;" class="row align-items-start justify-content-center">
-            <div style="margin-top: 3%;" v-for="genere in generi" :key="genere" class="col-md-3">
+            <div v-if="this.$store.state.currentLanguage == 'it'" style="margin-top: 3%;" v-for="genere in generi"
+                class="col-md-3">
+                <div class="form-check">
+                    <input style="zoom: 1.5;" class="form-check-input" type="checkbox" :value="genere"
+                        v-model="generiSelezionati">
+                    <label style="color: white; font-size: 1.2em;" :for="'checkbox_' + genere">
+                        {{ $t(genere) }}
+                    </label>
+                </div>
+            </div>
+            <div v-else style="margin-top: 3%;" v-for="genere in generis" class="col-md-3">
                 <div class="form-check">
                     <input style="zoom: 1.5;" class="form-check-input" type="checkbox" :value="genere"
                         v-model="generiSelezionati">
@@ -33,6 +43,10 @@ export default {
             generi: [
                 'Azione', 'Animazione', 'Avventura', 'Commedia', 'Crime', 'Documentario', 'Dramma', 'Famiglia',
                 'Fantascienza', 'Fantasy', 'Guerra', 'Horror', 'Mistero', 'Romance', 'Storia', 'Thriller'
+            ],
+            generis: [
+                'Action', 'Animation', 'Adventure', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family',
+                'Science Fiction', 'Fantasy', 'War', 'Horror', 'Mistery', 'Romance', 'History', 'Thriller'
             ],
             linguaSelezionata: this.$store.state.currentLanguage,
         };
